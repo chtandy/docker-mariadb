62a1f57693bf05653343ece1d3e46098966bde3a
chtandy <cht.andy@gmail.com>
2021-01-09 10:24:36 +0000

    update

--- conf/startup.sh
+++ conf/startup.sh
@@ -61,14 +61,14 @@ EOF
 
 		# set new User and Password
 		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
-            echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
-            echo "GRANT ALL ON \`$MYSQL_USER_DB\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+			echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+			echo "GRANT ALL ON \`$MYSQL_USER_DB\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
 		fi
 	else
 		# don`t need to create new database,Set new User to control all database.
 		if [ "$MYSQL_USER" != "" ] && [ "$MYSQL_USER_PWD" != "" ]; then
-            echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
-            echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
+			echo "[i] Creating user: $MYSQL_USER with password $MYSQL_USER_PWD"
+			echo "GRANT ALL ON *.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PWD';" >> $tfile
 		fi
 	fi
 
